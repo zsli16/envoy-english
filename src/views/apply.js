2937de6b87ca030815174a9511423e6113f4f449
Sue Li <sueli@post.harvard.edu>
2020-01-27 23:40:26 +0100

    update application height

--- src/views/apply.js
+++ src/views/apply.js
@@ -10,17 +10,15 @@ class Application extends Component {
     const style = {
       'position': 'relative',
       'minWidth':'100%',
-      'frameborder': 0, 
-      'scrolling': 'yes',
-      'seamless': 'seamless',
-      'height': 1998,
-      'width': '100%'
+      'height': 1880,
+      'width': '100%',
+      'overflow': 'hidden'
     };
 
     return (
       <>
       <Container>
-        <h1 style={{'text-align': 'center'}}>Register for Our Beta Trial</h1>
+        <h1>Register for Our Beta Trial</h1>
         <h2>Find Your Level, Pick a Mission, Get Started!</h2>
         <Row>
           <h4>We are currently accepting applications for our Beta trial. To participate, please fill out the form below. This form will allow us to identify your English level and recommend Missions that are most suitable to you. </h4>
@@ -29,12 +27,17 @@ class Application extends Component {
           <p>All of our Beta missions are Free! The only requirements are a computer with a camera, microphone, and a strong internet connection. Ready? Get started below.</p>
         </Row>
         <Row>
-          <HashLink to="/apply/#application"><FontAwesomeIcon id="down-arrow" fa-pulse color='lightgray' size="3x" icon={faChevronDown}/></HashLink>
+          <HashLink to="/apply/#application"><FontAwesomeIcon id="down-arrow" color='lightgray' size="3x" icon={faChevronDown}/></HashLink>
         </Row>
       </Container>
       <Container id="application">
         <h4>Application for English Course</h4>
-        <iframe src="https://services.cognitoforms.com/f/neg3yezHME-sqGKAznf3rA?id=36" style={style}/>
+        <iframe 
+          src="https://services.cognitoforms.com/f/neg3yezHME-sqGKAznf3rA?id=36"
+          scrolling='yes'
+          frameBorder='0'
+          seamless='seamless'
+          style={style}/>
       </Container>
       </>
     );
