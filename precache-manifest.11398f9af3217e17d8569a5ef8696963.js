1ef8d9276cbc0cba27e8e1782e4b6c1484319f1b
Sue Li <sueli@post.harvard.edu>
2020-01-29 21:12:20 +0100

    Updates

---
+++ precache-manifest.11398f9af3217e17d8569a5ef8696963.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06494bc52d98afa246b4ba3dafa58d3",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "62ac072a8a26bcd64a11",
+    "url": "/landing-page/static/css/main.3c8c8001.chunk.css"
+  },
+  {
+    "revision": "9d12a0bee574b8f839f8",
+    "url": "/landing-page/static/js/2.daa21d46.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/landing-page/static/js/2.daa21d46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62ac072a8a26bcd64a11",
+    "url": "/landing-page/static/js/main.1a813b37.chunk.js"
+  },
+  {
+    "revision": "635e04da24fa75f900b0",
+    "url": "/landing-page/static/js/runtime-main.e982c3ce.js"
+  },
+  {
+    "revision": "9aa47dc50319e898e326b51e5e302708",
+    "url": "/landing-page/static/media/missioncompleted.9aa47dc5.png"
+  },
+  {
+    "revision": "56c0e5b17b3aea9427985dc028bd5d19",
+    "url": "/landing-page/static/media/obstacle.56c0e5b1.png"
+  },
+  {
+    "revision": "c4ac95bd5d87ac82cac8adf4eead907f",
+    "url": "/landing-page/static/media/videocall.c4ac95bd.png"
+  }
+]);
