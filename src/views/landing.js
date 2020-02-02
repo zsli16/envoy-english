2c053f48414dd09edbb6d62e00140240113732e3
Sue Li <sueli@post.harvard.edu>
2020-02-02 19:16:35 +0100

    replace keys

--- src/views/landing.js
+++ src/views/landing.js
@@ -53,7 +53,7 @@ class LandingPage extends Component {
     return (
       <>
       <div style={{'position': 'absolute', 'top': '3%', 'right': '3%', 'display': 'flex', 'flexDirection': 'row-reverse', 'alignItems': 'center'}}>
-      <Link to='/apply'><Button primary>Sign up</Button></Link>
+      <Link to='/apply'><Button primary>{messages.Hero.button_2}</Button></Link>
       <FormControl variant="outlined">
         <Select value={this.state.language} onChange={this.handleChange}>
           <MenuItem value={'EN'}>EN</MenuItem>
@@ -73,36 +73,36 @@ class LandingPage extends Component {
         </Row>
       </Container>
       <Container id="how-it-works">
-        <h1>How It Works</h1>
-        <Row>The best way to master a language is to immerse yourself in real-life situations, which often requires traveling abroad. With Envoy English, you can now practice speaking English at your level without leaving your home. Each Mission requires you to learn specific vocabulary, phrases, and strategies. You will then use what you’ve learned to communicate with your teammates and complete a challenge.</Row>
+        <h1>{messages.HowItWorks.heading}</h1>
+        <Row>{messages.HowItWorks.description}</Row>
         <Row left tall>
           <img src={convo} alt="chat"/>
           <div>
-            <h3>Prepare for Your Mission</h3>
-            <h4>Study Your Mission Briefing to Learn Key Phrases and Vocabulary</h4>
-            <p>Your Mission Briefing contains the words, phrases, and information crucial to your success. Prepare to use these new phrases in your upcoming Mission, and you’ll sound like a native English speaker in no time! </p>
+            <h3>{messages.HowItWorks.section_header_1}</h3>
+            <h4>{messages.HowItWorks.section_tagline_1}</h4>
+            <p>{messages.HowItWorks.section_description_1}</p>
           </div>
         </Row>
         <Row right tall>
           <div>
-            <h3>Overcome Obstacles</h3>
-            <h4>Use Your Language Skills to Solve Problems with Your Teammates</h4>
-            <p>In order to succeed, you must use all of your English communication skills and the new vocabulary you have learned. You will have to work closely with your teammates to complete each challenge by uncovering clues, asking questions, and agreeing upon a course of action. Your native English instructor will guide you along the way, but the success of the Mission depends on you.</p>
+            <h3>{messages.HowItWorks.section_header_2}</h3>
+            <h4>{messages.HowItWorks.section_tagline_2}</h4>
+            <p>{messages.HowItWorks.section_description_2}</p>
           </div>
           <img src={obstacle} alt="obstacles"/>
         </Row>
         <Row left tall>
           <img src={missioncompleted} alt="levels" />
           <div>
-            <h3>Mission Accomplished!</h3>
-            <h4>Elevate Your Language Skills</h4>
-            <p>Each Mission is designed to increase your English speaking level. After the course, your instructor will send you personalized feedback with tips on how to keep improving. As you master the language, you will unlock higher level Missions. Are you up for the challenge?</p>
+            <h3>{messages.HowItWorks.section_header_3}</h3>
+            <h4>{messages.HowItWorks.section_tagline_3}</h4>
+            <p>{messages.HowItWorks.section_description_3}</p>
           </div>
         </Row>
       </Container>
       <ColoredContainer>
-        <h1>Our Curriculum</h1>
-        <Row>Envoy English uses digital immersion and interactive challenges to improve your English. Each Mission is designed to increase your level of fluency, coherency, interaction, and range of expression, as defined by the internationally recognized Common European Framework of Reference for Languages (CEFR). </Row>
+        <h1>{messages.Curriculum.header}</h1>
+        <Row>{messages.Curriculum.description}</Row>
         <Container>
           {Levels.map((l, i) => {
             return <Tube key={i}>
@@ -116,10 +116,10 @@ class LandingPage extends Component {
         </Container>
       </ColoredContainer>
       <Container>
-        <h1>Upcoming English Speaking Missions</h1>
+        <h1>{messages.UpcomingMissions.header}</h1>
         <Row>
           <p>
-            Ready to embark on your English speaking journey? Enroll in an upcoming Mission for free. We match you to a team with your right language level.
+            {messages.UpcomingMissions.description}
           </p>
         </Row>
         <Grid>
@@ -131,26 +131,26 @@ class LandingPage extends Component {
                 <h5>{mission.subtitle}</h5>
               </div>
               <div>
-                <p>Available for: {mission.levels}</p>
-                <Link to='/apply'><Button>Join Mission</Button></Link>
+                <p>{mission.levels}</p>
+                <Link to='/apply'><Button>{messages.UpcomingMissions.button_1}</Button></Link>
               </div>
             </Cell>
           })
         }
         </Grid>
         <Row>
-          <Button primary><Link to="/missions">View More Missions</Link></Button>
+          <Button primary><Link to="/missions">{messages.UpcomingMissions.button_2}</Link></Button>
         </Row>
       </Container>
       <ColoredContainer>
-        <h1>Gain Confidence in English</h1>
-        <Row>Do you struggle to express yourself in English? Have you had communication problems with international coworkers? Join a Mission today to gain the confidence you need to succeed in your personal and professional goals.</Row>
+        <h1>{messages.EmailForm.header}</h1>
+        <Row>{messages.EmailForm.description}</Row>
         <Row>
           <EmailForm/>
         </Row>
       </ColoredContainer>
       <Footer>
-        <h4>Contact Us</h4>
+        <h4>{messages.Contact.header}</h4>
         <p><span><FontAwesomeIcon color='white' size="1x" icon={faPhoneAlt}/></span>  +52 55 8421 9934 (Whatsapp)</p>
         <p><span><FontAwesomeIcon color='white' size="1x" icon={faEnvelope}/></span>  hello@envoyenglish.com</p>
       </Footer>
