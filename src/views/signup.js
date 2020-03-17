84a68a9fef0031f91281e3db8c7f0f12e9b0593a
Sue Li <sueli@post.harvard.edu>
2020-03-17 23:49:30 +0100

    fix missing import

--- src/views/signup.js
+++ src/views/signup.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Container, Row} from '../styles/layout';
 import {Button} from '../styles/ui-components';
 import Header from '../components/header';
+import SimplybookWidget from '../components/simplybookwidget';
 
 //content
 import EN from '../content/en.json';
@@ -18,11 +19,9 @@ class SignUpFreeTrial extends Component {
       messages: EN
     }
     this.simplybookButton = null;
-    // this.simplybookWidget = new SimplybookWidget({"widget_type":"iframe","url":"https://envoyenglish.simplybook.me","theme":"dainty","theme_settings":{"timeline_show_end_time":"1","sb_base_color":"#292076","secondary_color":"#e4ebf5","sb_text_color":"#a1a1a1","display_item_mode":"block","body_bg_color":"#ffffff","sb_background_image":"","dark_font_color":"#293b36","light_font_color":"#ffffff","sb_company_label_color":"#ffffff","sb_cancellation_color":"#ff7a93","hide_img_mode":"0"},"timeline":"modern_week","datepicker":"inline_datepicker","is_rtl":false,"app_config":{"predefined":[]}})
   }
 
   componentDidMount() {
-    this.setState({loaded: true});
     this.simplybookButton = new SimplybookWidget({"widget_type":"button","url":"https:\/\/envoyenglish.simplybook.me","theme":"dainty","theme_settings":{"timeline_show_end_time":"1","sb_base_color":"#292076","secondary_color":"#e4ebf5","sb_text_color":"#a1a1a1","display_item_mode":"block","body_bg_color":"#ffffff","sb_background_image":"","dark_font_color":"#293b36","light_font_color":"#ffffff","sb_company_label_color":"#ffffff","sb_cancellation_color":"#ff7a93","hide_img_mode":"0"},"timeline":"modern_week","datepicker":"inline_datepicker","is_rtl":false,"app_config":{"predefined":{"service":"4"}},"button_title":"Book now","button_background_color":"#292076","button_text_color":"#ffffff","button_position":"right","button_position_offset":"55%"});
   }
 
