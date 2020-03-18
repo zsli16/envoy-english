267d58463acbc5cb89fad9034c2343eb1c1e216f
Sue Li <sueli@post.harvard.edu>
2020-03-18 21:55:17 +0100

    fix componentwillunmount bug

--- src/views/signup.js
+++ src/views/signup.js
@@ -25,11 +25,6 @@ class SignUpFreeTrial extends Component {
     this.simplybookButton = new SimplybookWidget({"widget_type":"button","url":"https:\/\/envoyenglish.simplybook.me","theme":"dainty","theme_settings":{"timeline_show_end_time":"1","sb_base_color":"#292076","secondary_color":"#e4ebf5","sb_text_color":"#a1a1a1","display_item_mode":"block","body_bg_color":"#ffffff","sb_background_image":"","dark_font_color":"#293b36","light_font_color":"#ffffff","sb_company_label_color":"#ffffff","sb_cancellation_color":"#ff7a93","hide_img_mode":"0"},"timeline":"modern_week","datepicker":"inline_datepicker","is_rtl":false,"app_config":{"predefined":{"service":"4"}},"button_title":"Book now","button_background_color":"#292076","button_text_color":"#ffffff","button_position":"right","button_position_offset":"55%"});
   }
 
-  componentWillUnmount() {
-    const el = document.getElementById('simplybook');
-    el.parentNode.removeChild(el);
-  }
-
   handleChange(event) {
     const lang = event.target.value;
     this.setState({language: lang});
