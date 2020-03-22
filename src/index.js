18d6122228b927beef228e50a388adf884d0d28c
Sue Li <sueli@post.harvard.edu>
2020-03-22 19:10:42 +0100

    fix redirect

--- src/index.js
+++ src/index.js
@@ -25,16 +25,13 @@ function ScrollToTop() {
 ReactDOM.render(
   <BrowserRouter>
     <ScrollToTop/>
-    <Switch>
-      <Route exact path='/' component={App}/>
-      <Route exact path='/free-trial' component={SignupWidget}/>
-      <Route path='/missions' component={Missions}/>
-      <Route path='/trial-mission-briefing' component={TrialMissionBriefing}/>
-      <Route path='/free-class-code' component={FreeClassCode}/>
-      <Route path='/audio' component={ApplyAudio}/>
-      <Route path='/apply' component={Application}/>
-    </Switch>
-    <Redirect to='/'/>
+    <Route exact path='/' component={App}/>
+    <Route exact path='/free-trial' component={SignupWidget}/>
+    <Route exact path='/missions' component={Missions}/>
+    <Route exact path='/trial-mission-briefing' component={TrialMissionBriefing}/>
+    <Route exact path='/free-class-code' component={FreeClassCode}/>
+    <Route exact path='/audio' component={ApplyAudio}/>
+    <Route exact path='/apply' component={Application}/>
   </BrowserRouter>
   , document.getElementById('root')
 );
